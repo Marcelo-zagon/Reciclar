2fe09c8c742030247b59a3299a22267ac0eca5ad
marcelo-zagon <nikazagon@gmail.com>
2023-07-18 11:12:05 -0300

    Criando classes

--- Reciclar/src/App.java
+++
@@ -1,5 +0,0 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Deu no controle. Vamos reciclar!");
-    }
-}
---
+++ Reciclar/src/Man.java
@@ -0,0 +1,5 @@
+public class Man {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Deus no controle. Vamos reciclar!");
+    }
+}
